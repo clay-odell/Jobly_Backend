65a5b3f4d0eac22f8a63338b3c22a9ee09d101e2
Clay O'Dell <clay.odell.music@gmail.com>
2024-12-25 22:57:54 -0600

    db adjusted

--- config.js
+++ config.js
@@ -3,12 +3,11 @@
 require("dotenv").config();
 require("colors");
 
-// Print all environment variables to debug
-console.log("Environment Variables:", process.env);
-
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-const PORT = +process.env.DB_PORT || 3001;
+const PORT = +process.env.PORT || 3001;
+const DB_PORT = +process.env.DB_PORT || 5432;
 console.log(`PORT: ${PORT}`);
+console.log(`DB_PORT: ${DB_PORT}`);
 
 function getDatabaseUri() {
   return process.env.NODE_ENV === "test"
@@ -21,6 +20,7 @@ const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 13;
 console.log("Jobly Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
+console.log("DB_PORT:".yellow, DB_PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
 console.log("Database:".yellow, getDatabaseUri());
 console.log("---");
@@ -28,6 +28,7 @@ console.log("---");
 module.exports = {
   SECRET_KEY,
   PORT,
+  DB_PORT,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri
 };
