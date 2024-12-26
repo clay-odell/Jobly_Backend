28c665150cd7d40f38169a51019fde0d16a29e42
Clay O'Dell <clay.odell.music@gmail.com>
2024-12-25 23:03:14 -0600

    db adjusted again

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
 "use strict";
 const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
 
-const client = new Client({
-  connectionString: 'postgresql://postgres.xetbuzzfgtoeijwxzqaj:Supabase1302@aws-0-us-west-1.pooler.supabase.com:5432/postgres',
+const db = new Client({
+  connectionString: getDatabaseUri(),
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-client.connect()
-  .then(() => {
-    console.log('Connected to the database');
-    return client.end();
-  })
-  .catch(err => console.error('Connection error:', err.stack));
+db.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => {
+    console.error('Connection error:', err.stack);
+    process.exit(1);
+  });
+
+module.exports = db;
