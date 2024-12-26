2f98a3dc90e5f5052e3e79687347c3b9089fdf67
Clay O'Dell <clay.odell.music@gmail.com>
2024-12-25 22:47:56 -0600

    db adjusted

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.DB_PORT || 3001;
+console.log(PORT);
 
 function getDatabaseUri() {
   return process.env.NODE_ENV === "test"
